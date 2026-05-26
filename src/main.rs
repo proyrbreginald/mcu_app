6000092b1b85df27f56644dbe4ad2ef283fb92d2
reginald <yangrongbao@pudutech.com>
2026-05-26 15:29:24 +0800

    Add logging functionality to main.rs for LED control events

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,31 @@
 
 use mcu_hal::Peripherals;
 use mcu_rt as _;
+use mcu_rt::rtt_log;
 
 const LED_PIN: u16 = 13;
 const LED_MASK: u16 = 1 << LED_PIN;
 
 #[unsafe(no_mangle)]
 pub fn main() -> ! {
+    rtt_log!("mcu_rt booted!");
+    rtt_log!("mcu_app start!");
+
     let dp = unsafe { Peripherals::steal() };
     let gpio = dp.gpio;
 
     gpio.poerc()
         .modify(|r, w| unsafe { w.bits(r.bits() | LED_MASK) });
 
+    rtt_log!("led initialized!");
+
     loop {
         gpio.posrc().write(|w| unsafe { w.bits(LED_MASK) });
+        rtt_log!("led on!");
         delay(200_000);
 
         gpio.porrc().write(|w| unsafe { w.bits(LED_MASK) });
+        rtt_log!("led off!");
         delay(200_000);
     }
 }
